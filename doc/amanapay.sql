81574ba9fdb1328df0e998df3a6009c41a49d8dd
Mahmudul Hassan <hassan.01303@gmail.com>
2016-11-07 00:44:48 +0600

    db changes

--- doc/amanapay.sql
+++ doc/amanapay.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2016 at 11:58 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: Nov 06, 2016 at 06:43 PM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -43,6 +43,23 @@ INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `escrow`
+--
+
+CREATE TABLE `escrow` (
+  `id` int(11) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `seller` int(11) NOT NULL,
+  `buyer` int(11) NOT NULL,
+  `item` varchar(200) NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `status` enum('none','escrow','cancel') NOT NULL DEFAULT 'none',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `groups`
 --
 
@@ -64,6 +81,19 @@ INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `link` varchar(150) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login_attempts`
 --
 
@@ -73,6 +103,35 @@ CREATE TABLE `login_attempts` (
   `login` varchar(100) NOT NULL,
   `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `escrow_id` int(11) NOT NULL,
+  `payment_method` int(11) NOT NULL,
+  `amount` float(10,2) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  `status` tinyint(3) NOT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_preference`
+--
+
+CREATE TABLE `payment_preference` (
+  `id` int(11) NOT NULL,
+  `payment_method` varchar(200) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -96,6 +155,14 @@ CREATE TABLE `users` (
   `active` tinyint(1) UNSIGNED DEFAULT NULL,
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
+  `business` varchar(200) DEFAULT NULL,
+  `address1` varchar(200) DEFAULT NULL,
+  `address2` varchar(150) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `zip` varchar(20) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `payment_type` int(5) DEFAULT NULL,
   `company` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -104,10 +171,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1478431623, 1, 'Admin', 'istrator', 'ADMIN', '0'),
-(2, '::1', 'hassan', '$2y$08$7EYpllQqJTtsLWUAhHQXxuc7Shsyn0.dS85V9XWs12FO.wOAGEPI.', NULL, 'hassan@gmail.com', NULL, NULL, NULL, NULL, 1478422065, 1478431641, 1, 'a', 'a', 'a', 'a'),
-(3, '::1', 'test', '$2y$08$QYupM7c5ZN7iIerjUXOgye7kl45/28i0nJDTBTpdxW6K60ZGR.b9S', NULL, 'test@gmail.com', NULL, NULL, NULL, NULL, 1478431475, 1478431493, 1, 'test', 'test', 'test', '123');
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `business`, `address1`, `address2`, `city`, `state`, `zip`, `country`, `payment_type`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2y$08$rMUTXLxf2yeHHCJ9DffbJOEdSMUp9K61cqMHmoLMCPyo46KBpU20a', '', 'admin@gmail.com', '', NULL, NULL, NULL, 1268889823, 1478453795, 1, 'Admin', 'istrator', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ADMIN', '0'),
+(2, '::1', 'hassan', '$2y$08$7EYpllQqJTtsLWUAhHQXxuc7Shsyn0.dS85V9XWs12FO.wOAGEPI.', NULL, 'hassan@gmail.com', NULL, NULL, NULL, NULL, 1478422065, 1478453502, 1, 'a', 'a', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'a', 'a'),
+(3, '::1', 'test', '$2y$08$QYupM7c5ZN7iIerjUXOgye7kl45/28i0nJDTBTpdxW6K60ZGR.b9S', NULL, 'test@gmail.com', NULL, NULL, NULL, NULL, 1478431475, 1478431493, 1, 'test', 'test', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'test', '123');
 
 -- --------------------------------------------------------
 
@@ -141,15 +208,39 @@ ALTER TABLE `ci_sessions`
   ADD KEY `ci_sessions_timestamp` (`timestamp`);
 
 --
+-- Indexes for table `escrow`
+--
+ALTER TABLE `escrow`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_preference`
+--
+ALTER TABLE `payment_preference`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -172,15 +263,35 @@ ALTER TABLE `users_groups`
 --
 
 --
+-- AUTO_INCREMENT for table `escrow`
+--
+ALTER TABLE `escrow`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment_preference`
+--
+ALTER TABLE `payment_preference`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
